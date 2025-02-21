99c20c06041cc7c16589b39d5e0ef8f45962e494
suleiman hamza <hamzasuleiman530@gmail.com>
2025-02-21 21:33:36 +0100

    tested computed getter & setter

--- pages/about.vue
+++ pages/about.vue
@@ -1,3 +1,23 @@
+<script lang="ts" setup>
+import { ref, computed } from 'vue';
+
+const name = ref('cb wg');
+const formattedName = computed({
+    get() {
+        return name.value
+    },
+    set(value) {
+        return name.value = value.toLowerCase().replace(/\b\w\g/, c => c.toUpperCase())
+    }
+
+})
+</script>
 <template>
     <p>About Pages</p>
+    <section>
+        <h2>Computed Getter & Setter</h2>
+        <label>Full name</label>
+        <input type="text" v-model="formattedName">
+    </section>
+    <p>{{ name }}</p>
 </template>
